c3ca1d1d131465ba2f2b1bf2a57292603f3d5ed9
pubalisen <pubali.sen@sjsu.edu>
2014-04-09 08:27:38 -0700

    Final Commit

--- library/src/main/java/edu/sjsu/cmpe/library/LibraryService.java
+++ library/src/main/java/edu/sjsu/cmpe/library/LibraryService.java
@@ -1,4 +1,12 @@
 package edu.sjsu.cmpe.library;
+//http://54.215.133.131:61680/
+
+
+import java.net.MalformedURLException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import javax.jms.JMSException;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -12,6 +20,7 @@ import com.yammer.dropwizard.views.ViewBundle;
 import edu.sjsu.cmpe.library.api.resources.BookResource;
 import edu.sjsu.cmpe.library.api.resources.RootResource;
 import edu.sjsu.cmpe.library.config.LibraryServiceConfiguration;
+import edu.sjsu.cmpe.library.repository.AsyncReceiver;
 import edu.sjsu.cmpe.library.repository.BookRepository;
 import edu.sjsu.cmpe.library.repository.BookRepositoryInterface;
 import edu.sjsu.cmpe.library.ui.resources.HomeResource;
@@ -23,7 +32,6 @@ public class LibraryService extends Service<LibraryServiceConfiguration> {
     public static void main(String[] args) throws Exception {
 	new LibraryService().run(args);
     }
-
     @Override
     public void initialize(Bootstrap<LibraryServiceConfiguration> bootstrap) {
 	bootstrap.setName("library-service");
@@ -37,18 +45,46 @@ public class LibraryService extends Service<LibraryServiceConfiguration> {
 	// This is how you pull the configurations from library_x_config.yml
 	String queueName = configuration.getStompQueueName();
 	String topicName = configuration.getStompTopicName();
-	log.debug("{} - Queue name is {}. Topic name is {}",
-		configuration.getLibraryName(), queueName,
-		topicName);
+	String apollouser=configuration.getApolloUser();
+	String apolloPassword=configuration.getApolloPassword();
+	String apollohost=configuration.getApolloHost();
+	int apolloPort=configuration.getApolloPort();
+	log.debug("Queue name is {}. Topic name is {}. User is {}, password is {}. host is {}. port is {}", queueName,topicName,apollouser,apolloPassword,apollohost,apolloPort);
 	// TODO: Apollo STOMP Broker URL and login
+	// Do from the library_a_config.yml file
+	
+	//--------------------------------------------------------------------------
 
 	/** Root API */
 	environment.addResource(RootResource.class);
 	/** Books APIs */
-	BookRepositoryInterface bookRepository = new BookRepository();
+	BookRepositoryInterface bookRepository = new BookRepository(configuration);
+
 	environment.addResource(new BookResource(bookRepository));
 
 	/** UI Resources */
 	environment.addResource(new HomeResource(bookRepository));
+	//environment.addResource(new AsyncReceiver(configuration, bookrepoactions));
+	ExecutorService executor = Executors.newFixedThreadPool(1);
+	final AsyncReceiver asyncReceiver=new AsyncReceiver(configuration, bookRepository);
+    Runnable backgroundTask = new Runnable() {
+
+	    @Override
+	    public void run() {
+	    	try {
+				try {
+					asyncReceiver.listernerMsg();
+				} catch (MalformedURLException e) {
+					e.printStackTrace();
+				}
+			} catch (JMSException e) {
+				e.printStackTrace();
+			}
+	    }
+
+	};
+	executor.execute(backgroundTask);
+//	AsyncReceiver async=new AsyncReceiver(configuration, bookrepoactions);
+//	async.listernerMsg();
     }
 }
--- library/src/main/java/edu/sjsu/cmpe/library/config/LibraryServiceConfiguration.java
+++ library/src/main/java/edu/sjsu/cmpe/library/config/LibraryServiceConfiguration.java
@@ -10,13 +10,81 @@ public class LibraryServiceConfiguration extends Configuration {
     @JsonProperty
     private String stompQueueName;
 
+
+	@NotEmpty
+    @JsonProperty
+    private String apolloPassword;
+    
+    @NotEmpty
+    @JsonProperty
+    private String apolloHost;
+    
+    @JsonProperty
+    private int apolloPort;
+    
+    @NotEmpty
+    @JsonProperty
+    private String apolloUser;
+    
+    /**
+	 * @return the apolloUser
+	 */
+	public String getApolloUser() {
+		return apolloUser;
+	}
+
+	/**
+	 * @param apolloUser the apolloUser to set
+	 */
+	public void setApolloUser(String apolloUser) {
+		this.apolloUser = apolloUser;
+	}
+
+	/**
+	 * @return the apolloPassword
+	 */
+	public String getApolloPassword() {
+		return apolloPassword;
+	}
+
+	/**
+	 * @param apolloPassword the apolloPassword to set
+	 */
+	public void setApolloPassword(String apolloPassword) {
+		this.apolloPassword = apolloPassword;
+	}
+
+	/**
+	 * @return the apolloHost
+	 */
+	public String getApolloHost() {
+		return apolloHost;
+	}
+
+	/**
+	 * @param apolloHost the apolloHost to set
+	 */
+	public void setApolloHost(String apolloHost) {
+		this.apolloHost = apolloHost;
+	}
+
+	/**
+	 * @return the apolloPort
+	 */
+	public int getApolloPort() {
+		return apolloPort;
+	}
+
+	/**
+	 * @param apolloPort the apolloPort to set
+	 */
+	public void setApolloPort(int apolloPort) {
+		this.apolloPort = apolloPort;
+	}
+    
     @NotEmpty
     @JsonProperty
     private String stompTopicName;
-
-    @NotEmpty
-    @JsonProperty
-    private String libraryName;
 
     /**
      * @return the stompQueueName
@@ -46,20 +114,5 @@ public class LibraryServiceConfiguration extends Configuration {
      */
     public void setStompTopicName(String stompTopicName) {
 	this.stompTopicName = stompTopicName;
-    }
-
-    /**
-     * @return the libraryName
-     */
-    public String getLibraryName() {
-	return libraryName;
-    }
-
-    /**
-     * @param libraryName
-     *            the libraryName to set
-     */
-    public void setLibraryName(String libraryName) {
-	this.libraryName = libraryName;
     }
 }
